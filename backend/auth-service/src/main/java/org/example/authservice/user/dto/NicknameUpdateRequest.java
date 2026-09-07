49a487ae8a4b819b21264f58616cc3f706c0633f
ChoiSeungHwan <csh980930@naver.com>
2026-09-07 19:42:26 +0900

    feat: 닉네임 변경 요청 DTO 추가

---
+++ backend/auth-service/src/main/java/org/example/authservice/user/dto/NicknameUpdateRequest.java
@@ -0,0 +1,19 @@
+package org.example.authservice.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NicknameUpdateRequest {
+
+    @Schema(description = "변경할 새 닉네임", example = "개발자")
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+}
